c60ea5d15b1dd7d81b7f19f9da7f9d7bfbe52a07
Tim Stella <tstella@denstark.net>
2017-05-03 14:55:57 -0700

    Refactored to use discord command bot, added fortune command and TODO

--- commands.rb
+++ commands.rb
@@ -1,34 +1,39 @@
-_prefix = 'p!'
-
 $commands = {
   "goodshit" => "Good shit copypasta",
   "help" => "this help message",
   "xkcd" => "Get a random xkcd",
   "qwantz" => "Get a random dinosaurcomic",
+  "fortune" => "Get a drop of wisdom"
 }
 
-def goodshit
-  return "👌👀👌👀👌👀👌👀👌👀 good shit go౦ԁ sHit👌 thats ✔ some good👌👌shit right👌👌there👌👌👌 right✔there ✔✔if i do ƽaү so my self 💯 i say so 💯 thats what im talking about right there right there (chorus: ʳᶦᵍʰᵗ ᵗʰᵉʳᵉ) mMMMMᎷМ💯 👌👌 👌НO0ОଠOOOOOОଠଠOoooᵒᵒᵒᵒᵒᵒᵒᵒᵒ👌 👌👌 👌 💯 👌 👀 👀 👀 👌👌Good shit"
+$puck.command :goodshit do |event|
+  "👌👀👌👀👌👀👌👀👌👀 good shit go౦ԁ sHit👌 thats ✔ some good👌👌shit right👌👌there👌👌👌 right✔there ✔✔if i do ƽaү so my self 💯 i say so 💯 thats what im talking about right there right there (chorus: ʳᶦᵍʰᵗ ᵗʰᵉʳᵉ) mMMMMᎷМ💯 👌👌 👌НO0ОଠOOOOOОଠଠOoooᵒᵒᵒᵒᵒᵒᵒᵒᵒ👌 👌👌 👌 💯 👌 👀 👀 👀 👌👌Good shit"
 end
 
-def help
+$puck.command :help do |event|
   output = "```"
   $commands.each do |key, val|
     output += "p!#{key} - #{val}\n"
   end
   output += "```"
-  return output
-
+  output
 end
 
-def xkcd
+$puck.command :xkcd do |event|
   maxXKCD = 1821
   num = rand(1..maxXKCD)
-  return "https://xkcd.com/#{num}/"
+  "https://xkcd.com/#{num}/"
 end
 
-def qwantz
+$puck.command :qwantz do |event|
   max = 3128
   num = rand(1..max)
-  return "http://www.qwantz.com/index.php?comic=#{num}"
+  "http://www.qwantz.com/index.php?comic=#{num}"
+end
+
+$puck.command :fortune do |event|
+  output = "```"
+  output += %x(fortune)
+  output += "```"
+  output
 end
