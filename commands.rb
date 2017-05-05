1946580559b9257d8218cdc701f3d59494df23c1
Tim Stella <tstella@denstark.net>
2017-05-05 16:30:31 -0700

    Moved to sqlite3 for quotes, added gemfile

--- commands.rb
+++ commands.rb
@@ -46,16 +46,19 @@ $puck.command :quote do |event, *args|
   if args[0] == "add"
     args.shift
     quote = args.join(' ')
-    open(QUOTES_FILE, 'a') do |f|
-      f.puts quote
-    end
+    $db.execute("INSERT INTO quotes(author, quote, date)
+    VALUES(?, ?, ?);", [event.user.name, quote, Time.now.to_i])
     "Quote added!"
   elsif args.empty?
-    chosen_quote = nil
-    File.foreach(QUOTES_FILE).each_with_index do |line, number|
-      chosen_quote = line if rand < 1.0/(number+1)
-    end
-    "#{chosen_quote}"
+    rows = $db.execute("SELECT count() from quotes;")
+    count = rows[0][0]
+    rand_row = rand(1..count)
+    rows = $db.execute("SELECT author, quote, date FROM quotes WHERE id = ?", [rand_row])
+    author = rows[0][0]
+    quote = rows[0][1]
+    date = Time.at(rows[0][2])
+    "#{quote}\n
+    Added by **#{author}** at #{date}"
   end
 end
 
--- puck.rb
+++ puck.rb
@@ -1,5 +1,8 @@
 require 'discordrb'
+require 'sqlite3'
 require_relative 'config.rb'
+$db = SQLite3::Database.new QUOTES_DB
+
 
 $puck = Discordrb::Commands::CommandBot.new token: DISCORD_TOKEN, client_id: DISCORD_CLIENT_ID, prefix: 'p!'
 require_relative 'commands.rb'
