c2ca02e20f8f79921ced2c35973658407759e686
Tim Stella <tstella@denstark.net>
2018-08-26 22:31:52 -0700

    Added 8ball command w/ Ray

--- commands.rb
+++ commands.rb
@@ -7,6 +7,7 @@ $commands = {
   "quote" => "Get a quote or add a quote with 'quote add'",
   "trout" => "Slap someone with a trout",
   "coin" => "Get a coinflip",
+  "8ball" => "Ask the magic 8 ball a question",
 }
 
 $puck.command :goodshit do |event|
@@ -21,6 +22,32 @@ $puck.command :help do |event|
   output += "```"
   event.user.pm(output)
   nil
+end
+
+$puck.command :'8ball' do |event|
+  answers = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes - definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful.",
+  ]
+  answers.sample
 end
 
 $puck.command :xkcd do |event|
