a6bb1e69af963aa07d19c849a25ab1e01dbc7729
Tim Stella <tstella@denstark.net>
2018-09-18 18:45:21 -0700

    Added weather command

--- commands/weather.rb
+++ commands/weather.rb
@@ -9,17 +9,37 @@ $puck.command :weather do |event, *args|
       embed.colour = 0xff0000
       embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: "Error")
       embed.timestamp = Time.at(Time.now)
-      embed.description = "Error: #{city} not found."
+      embed.description = "#{city} not found."
     end
     return
   end
   coords = search.first.coordinates
   url = "https://api.darksky.net/forecast/#{DARKSKY_API_KEY}/#{coords[0]},#{coords[1]}"
+  puts "Pulling: #{url}"
   uri = URI(url)
   response = Net::HTTP.get(uri)
   forecast = JSON.parse(response)
-  temp = forecast['currently']['temperature'].to_i
-  summary = forecast['currently']['summary']
+  currently = forecast['currently']
+  daily = forecast['daily']
+  temp = currently['temperature'].to_i
+  hourly = forecast['hourly']
+  summary = currently['summary']
+  hourlySummary = hourly['summary']
+  icons = {
+      'clear-day' => ':sunny:',
+      'clear-night' => ':night_with_stars:',
+      'rain' => ':cloud_rain:',
+      'snow' => ':cloud_snow:',
+      'sleet' => ':cloud_rain:',
+      'wind' =>  ':dash:',
+      'fog' => ':foggy:',
+      'cloudy' => ':cloud:',
+      'partly-cloudy-day' => ':white_sun_cloud:',
+      'partly-cloudy-night' => ':cloud:',
+  }
+  icon = icons[currently['icon']]
+
+
   colour = nil
 
   case temp
@@ -33,8 +53,9 @@ $puck.command :weather do |event, *args|
     colour = 0xA1B0F4
   end
 
-  desc = ":thermometer: #{temp}°F\n"
-  desc += summary
+  desc = ":thermometer: #{temp}°F #{icon} #{summary}\n\n"
+  desc += "**Today's Forecast**\n"
+  desc += hourlySummary
   event.channel.send_embed do |embed|
     embed.colour = colour
     embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: city)
