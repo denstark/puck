63d6e6415ee06df3e14c828dc32091f5353b5a96
Tim Stella <tstella@denstark.net>
2018-09-19 21:45:07 -0700

    Updated doubt image

--- commands/doubt.rb
+++ commands/doubt.rb
@@ -3,7 +3,7 @@ $commands["doubt"] = "Press X to express doubt."
 $puck.command :doubt do |event, *args|
 
   event.channel.send_embed do |embed|
-    embed.image = Discordrb::Webhooks::EmbedImage.new(url: "https://cdn.discordapp.com/attachments/193255335253901312/492191251995820047/Doubt.jpg")
+    embed.image = Discordrb::Webhooks::EmbedImage.new(url: "https://cdn.discordapp.com/attachments/84435142470270976/492193424351559715/Doubt.png")
   end
 
 end
