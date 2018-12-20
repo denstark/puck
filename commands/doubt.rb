cd74067b6c693d76cc4c9c0e7925c98ad61b91f9
Tim Stella <tstella@denstark.net>
2018-12-20 03:54:12 -0800

    Fixed doubt command to have spook flair only during October

--- commands/doubt.rb
+++ commands/doubt.rb
@@ -1,10 +1,11 @@
 $commands["doubt"] = "Press X to express doubt."
 
 $puck.command :doubt do |event, *args|
-
   event.channel.send_embed do |embed|
-    embed.image = Discordrb::Webhooks::EmbedImage.new(url: "https://cdn.discordapp.com/attachments/84435142470270976/492193424351559715/Doubt.png")
-    #embed.image = Discordrb::Webhooks::EmbedImage.new(url: "https://cdn.discordapp.com/attachments/84493464808480768/496290515927564297/Doot.PNG")
+    if Time.now.month == 10
+      embed.image = Discordrb::Webhooks::EmbedImage.new(url: "https://cdn.discordapp.com/attachments/84493464808480768/496290515927564297/Doot.PNG")
+    else
+      embed.image = Discordrb::Webhooks::EmbedImage.new(url: "https://cdn.discordapp.com/attachments/84435142470270976/492193424351559715/Doubt.png")
+    end
   end
-
 end
