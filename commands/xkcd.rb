65affa712acbe311eedc8211dc0237a3cc747c58
Tim Stella <tstella@denstark.net>
2018-12-21 20:17:08 -0800

    Updated xkcd command to use their exposed json

--- commands/xkcd.rb
+++ commands/xkcd.rb
@@ -1,7 +1,11 @@
 $commands["xkcd"] = "Get a random xkcd comic"
 
 $puck.command :xkcd do |event|
-  maxXKCD = 1821
+  XKCD_JSON_URL = 'https://xkcd.com/info.0.json'
+  uri = URI(XKCD_JSON_URL)
+  response = Net::HTTP.get(uri)
+  dict = JSON.parse(response)
+  maxXKCD = dict['num']
   num = rand(1..maxXKCD)
   "https://xkcd.com/#{num}/"
 end
