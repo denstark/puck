8506483efe2ecbf0228345a592d992b706c09202
rpruazol <rpruazol@gmail.com>
2024-01-08 19:30:59 -0500

    add personal ranking if not in top 10

--- commands.rb
+++ commands.rb
@@ -1,6 +1,9 @@
 $commands = Hash.new
 require_all "commands/*.rb"
 
+logger = Logger.new(STDOUT)
+logger.level = Logger::WARN
+
 $puck.command :help do |event|
   output = "```"
   $commands.each do |key, val|
@@ -8,5 +11,6 @@ $puck.command :help do |event|
   end
   output += "```"
   event.user.pm(output)
+  logger.info "User-input: #{output}" 
   nil
 end
--- commands/top.rb
+++ commands/top.rb
@@ -1,20 +1,44 @@
 $commands["top"] = "See the top chat leaders!"
 
+
+
+
 $puck.command :top do |event, *args|
   serverId = event.user.server.id
   serverName = event.user.server.name
-  top = DB[:top]
-  rows = top.where(server: serverId).reverse_order(:score).limit(10)
+  top = DB[:top].where(server:serverId).reverse_order(:score)
+  rows = top.where(server:serverId).reverse_order(:score).limit(10)
   num = 1
   output = ''
-  rows.each { |user|
-    output += "##{num}: #{user[:name]} - #{user[:score]}\n"
-    num += 1
-  }
 
+  me = my_rank(event)
+  
+  rows.each { |user|
+  output += "##{num}: #{user[:name]} - #{user[:score]}\n"
+  num += 1
+  }
   event.channel.send_embed do |embed|
-    embed.title = ":chart_with_upwards_trend: **Top chatters for #{serverName}**"
+    embed.title = "
+    :chart_with_upwards_trend: **Top chatters for #{serverName}**
+    
+    ~##{me["rank"]}: #{me["name"]} - #{me["score"]}~
+    "
+
     embed.colour = 0x42f4c2
     embed.description = output
   end
+end
+
+def my_rank(event)
+  me = {"name" => event.user.name}
+  rows = DB[:top].where(server:event.user.server.id).reverse_order(:score).limit(100)
+  num = 0
+  rows.each { |user|
+    num += 1
+    if user[:name] == event.user.name
+      me["rank"] = num
+      me["score"] = user[:score]
+    end
+}
+  return me
 end
